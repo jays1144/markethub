39c1deb11ac58c9d25d28774d4d79f2f8fdb6f0f
naraspc <wodnrtkfka@naver.com>
2024-02-20 09:24:07 +0900

    token init

--- src/main/java/org/hanghae/markethub/domain/purchase/controller/PaymentController.java
+++ src/main/java/org/hanghae/markethub/domain/purchase/controller/PaymentController.java
@@ -143,19 +143,28 @@ public class PaymentController {
     }
 
     @PostMapping("/api/payment/token")
-    public String getAccessToken(@RequestBody PaymentRequestDto.getToken tokenData) throws {
+    public String getAccessToken(@RequestBody PaymentRequestDto.getToken tokenData) throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
-
+        ObjectMapper object = new ObjectMapper();
         String url = "https://api.iamport.kr/users/getToken";
 
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
+
+        String requestData = object.writeValueAsString(tokenData);
+        HttpEntity<String> request1 = new HttpEntity<>(requestData, headers);
         HttpEntity<PaymentRequestDto.getToken> request = new HttpEntity<>(tokenData, headers);
+        System.out.println(request1 + " request1");
+        System.out.println(request + " :request");
+        System.out.println(tokenData + " : tokenData 직렬화하지않음 ");
+        System.out.println(object.writeValueAsString(tokenData) + " 토큰데이터 직렬화");
+        System.out.println(requestData + " 직렬화 스트링타입으로 저장");
 
-
+        ResponseEntity<IamportResponseDto> response1 = restTemplate.postForEntity(url, request1, IamportResponseDto.class);
         ResponseEntity<IamportResponseDto> response = restTemplate.postForEntity(url, request, IamportResponseDto.class);
+        System.out.println(response1.getBody().response().access_token());
 
         if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
             System.out.println("토큰발급완료");
